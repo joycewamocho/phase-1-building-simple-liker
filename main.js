7ccb9845c5bc5059923c84e0feaf09e5e7b4fc7d
joyce wamocho <joywamo@gmail.com>
2024-10-11 13:48:32 +0300

    liker-app

--- main.js
+++ main.js
@@ -3,9 +3,39 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.addEventListener("DOMContentLoaded",()=>{
+const hearts = document.getElementsByClassName("like-glyph");
+for(const heart of hearts ){
+  heart.addEventListener("click", (event)=>{
+    event.preventDefault();
+    
+    
+    mimicServerCall() 
+    .then(()=>{
+      if(event.target.textContent === EMPTY_HEART){
+        event.target.textContent = FULL_HEART
+        event.target.classList.add("activated-heart")
+      }else{
+        event.target.textContent = EMPTY_HEART
+        event.target.classList.remove("activated-heart")
+      }
+    })
+    .catch((error)=>{
+      let modal  = document.querySelector(".hidden")
+      modal.classList.remove("hidden")
+      errorMessage =modal.querySelector("p")
+      errorMessage.textContent = error.message
+      console.log(errorMessage)
+      
+      setTimeout(()=>{
+        modal.classList.add("hidden")
+      },3000);
+    })
+    
+  })
+}
 
-
-
+})
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
